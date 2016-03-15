78419064b76bec048113d223f6e46193ed0bbce5
Majid Mosayebi <majid.mosayebi@gmail.com>
2016-03-15 20:10:55 +0000

    sq

--- structure_factor_2.py
+++ structure_factor_2.py
@@ -270,7 +270,7 @@ elif mode == 24 :
     for i in range(N_hub):  
         xx = [] 
         for p in range(3):
-            while not p_type[cnt] == 14:
+            while not p_type[cnt] == 11:
                 cnt += 1
                 print cnt, p_type[cnt]
             xx.append(x[cnt,:]) 
