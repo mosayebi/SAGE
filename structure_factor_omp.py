38d3aab706e4d9574a6efe1a55d7d87d1c869393
Majid Mosayebi <majid.mosayebi@gmail.com>
2016-03-15 22:41:31 +0000

    sq

--- structure_factor_omp.py
+++ structure_factor_omp.py
@@ -113,7 +113,7 @@ else:
 # max_timestep = 1e10
 # traj_file = '/Users/mm15804/scratch/SAGE/psi3_test/dump_0.05.lammpstrj'
 traj_data = hub.read_dump(traj_file, min_timestep, max_timestep)
-traj_data = traj_data[-5:]
+traj_data = traj_data[-10:]
 sq_file = traj_file+'.hubhub.sq.03'
 
 
